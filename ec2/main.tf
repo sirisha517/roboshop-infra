608e8f88daeeeb81baf8b265efc78ca750ed373c
sirisha517 <sai.sirifriends8790@gmail.com>
2023-02-28 17:48:00 +0530

    updating roboshop-infra

--- ec2/main.tf
+++ ec2/main.tf
@@ -4,54 +4,62 @@ data "aws_ami" "ami" {
   owners           = ["973714476881"] //OWNER
 }
 
+
 resource "aws_instance" "ec2" {
-  ami = data.aws_ami.ami.image_id
-  instance_type = var.instance_type
+  ami                    = data.aws_ami.ami.image_id
+  instance_type          = var.instance_type
   vpc_security_group_ids = [aws_security_group.sg.id]
-  tags ={
-    Name =  var.component
+  tags = {
+    Name = var.component
   }
 }
-resource "null_resource" "null" {
+
+resource "null_resource" "provisioner" {
   provisioner "remote-exec" {
+
     connection {
-      host = aws_instance.ec2.public_ip
-      user = "root"
+      host     = aws_instance.ec2.public_ip
+      user     = "centos"
       password = "DevOps321"
     }
+
     inline = [
-      "git clone https://github.com/sirisha517/roboshop-shell",
+      "git clone https://github.com/raghudevopsb71/roboshop-shell",
       "cd roboshop-shell",
       "sudo bash ${var.component}.sh"
     ]
+
   }
+
 }
+
 resource "aws_security_group" "sg" {
   name        = "${var.component}-${var.env}-sg"
-  description = "Allow  TLS inbound traffic"
+  description = "Allow TLS inbound traffic"
 
   ingress {
-    description      = "ALL"
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
+    description = "ALL"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = {
     Name = "${var.component}-${var.env}-sg"
   }
 }
+
 resource "aws_route53_record" "record" {
-  zone_id = "Z0640963AEF75NOLDKGO"
-  name    = "${var.component}.devops517test.online"
+  zone_id = "Z103214126L48SQW30RSR"
+  name    = "${var.component}-dev.devopsb71.online"
   type    = "A"
   ttl     = 30
   records = [aws_instance.ec2.private_ip]
--- var.tf
+++ var.tf
@@ -1,44 +1,48 @@
-variable "instance" {
+variable "instances" {
   default = {
     frontend = {
       name = "frontend"
       type = "t3.micro"
     }
-    Mongodb = {
-      name = "Mongodb"
-      type = "t3.micro"
-    }
-    user = {
-      name = "user"
-      type = "t3.micro"
-    }
-    Rabbitmq = {
-      name = "Rabbitmq"
+    mongodb = {
+      name = "mongodb"
       type = "t3.micro"
     }
     catalogue = {
       name = "catalogue"
       type = "t3.micro"
     }
-    mysql = {
-      name = "mysql"
-      type = "t3.micro"
-    }
-    payment = {
-      name = "payment"
-      type = "t3.micro"
-    }
-    shipping = {
-      name = "shipping"
-      type = "t3.micro"
-    }
     redis = {
       name = "redis"
+      type = "t3.micro"
+    }
+    user = {
+      name = "user"
       type = "t3.micro"
     }
     cart = {
       name = "cart"
       type = "t3.micro"
+    }
+    mysql = {
+      name     = "mysql"
+      type     = "t3.micro"
+      password = "RoboShop@1"
+    }
+    shipping = {
+      name     = "shipping"
+      type     = "t3.micro"
+      password = "RoboShop@1"
+    }
+    rabbitmq = {
+      name     = "rabbitmq"
+      type     = "t3.micro"
+      password = "roboshop123"
+    }
+    payment = {
+      name     = "payment"
+      type     = "t3.micro"
+      password = "roboshop123"
     }
   }
 }
