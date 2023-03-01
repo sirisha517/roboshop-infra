a20beff417afc4a42fba8031d9eda05b3391d687
sirisha517 <sai.sirifriends8790@gmail.com>
2023-03-01 14:25:31 +0530

    updating roboshop-infra

--- ec2/main.tf
+++ ec2/main.tf
@@ -7,7 +7,7 @@ data "aws_ami" "ami" {
 resource "aws_instance" "ec2" {
   ami = data.aws_ami.ami.image_id
   instance_type = var.instance_type
-  vpc_security_group_ids = [var.sg_id]
+  vpc_security_group_ids = [aws_security_group.sg.id]
   tags ={
     Name =  var.component
   }
