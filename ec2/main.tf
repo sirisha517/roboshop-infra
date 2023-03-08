62a3d0e7c9b7b00377bc84cb72d0372623ff0667
sirisha517 <sai.sirifriends8790@gmail.com>
2023-03-08 14:29:08 +0530

    test

--- ec2/main.tf
+++ ec2/main.tf
@@ -17,7 +17,7 @@ resource "null_resource" "provisioner" {
       password = "DevOps321"
     }
     inline = [
-      "ansible-pull -i localhost, -U https://github.com/sirisha517/roboshop-ansible roboshop.yml -e role_name=${var.component} -e env=${env}"
+      "ansible-pull -i localhost, -U https://github.com/sirisha517/roboshop-ansible roboshop.yml -e role_name=${var.component} -e env=${var.env}"
 
     ]
 
