50c7d290d13fa4525e040f8436ba966a3d647a21
sirisha517 <sai.sirifriends8790@gmail.com>
2023-03-20 23:33:24 +0530

    vpc

--- main.tf
+++ main.tf
@@ -1,9 +1,4 @@
-module "ec2" {
-  for_each      = var.instances
-  source        = "./ec2"
-  component     = each.value["name"]
-  instance_type = each.value["type"]
-  env           = var.env
-  monitor = try(each.value["monitor"], false)
-
+module "vpc" {
+  source = "git::https://github.com/sirisha517/tf-module-vpc.git"
+  for_each = var.vpc
 }
