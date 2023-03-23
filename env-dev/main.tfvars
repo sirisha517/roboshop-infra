7f257016327efacf40727c0a3987b8249d02ae45
sirisha517 <sai.sirifriends8790@gmail.com>
2023-03-23 19:12:07 +0530

    vpc

--- env-dev/main.tfvars
+++ env-dev/main.tfvars
@@ -1,6 +1,6 @@
 vpc = {
   main = {
-    cidr = "10.0.0.0/16"
+    vpc_cidr = "10.0.0.0/16"
     public_subnets = {
       public = {
         name = "public"
--- main.tf
+++ main.tf
@@ -1,4 +1,5 @@
 module "vpc" {
+  vpc_cidr = each.value["vpc_cidr"]
   source = "git::https://github.com/sirisha517/tf-module-vpc.git"
   for_each = var.vpc
 }
