0ee04048e6cdec64a36e552a33ba81502961f539
sirisha517 <sai.sirifriends8790@gmail.com>
2023-03-23 19:24:48 +0530

    vpc

--- main.tf
+++ main.tf
@@ -1,6 +1,7 @@
 module "vpc" {
   source = "git::https://github.com/sirisha517/tf-module-vpc.git"
+  env = var.env
   for_each = var.vpc
-  vpc_cidr = each.value["vpc_cidr"]
+  vpc_cidr = each.value["cidr"]
 
 }
