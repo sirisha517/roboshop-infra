3b4cb72e54321a8ce241c38100368b1dcaeb6b7c
sirisha517 <sai.sirifriends8790@gmail.com>
2023-03-27 08:20:10 +0530

    vpc

--- main.tf
+++ main.tf
@@ -1,15 +1,16 @@
 module "vpc" {
-  source = "git::https://github.com/sirisha517/tf-module-vpc.git"
-  env    = var.env
-  tags   = var.tags
-  default_vpc_id = var.default_vpc_id
+  source              = "git::https://github.com/raghudevopsb71/tf-module-vpc.git"
+  env                 = var.env
+  tags                = var.tags
   default_route_table = var.default_route_table
-  for_each = var.vpc
-  vpc_cidr = each.value["vpc_cidr"]
-  public_subnets = each.value["public_subnets"]
-  private_subnets = each.value["private_subnets"]
+  default_vpc_id      = var.default_vpc_id
 
+  for_each        = var.vpc
+  vpc_cidr        = each.value["vpc_cidr"]
+  public_subnets  = each.value["public_subnets"]
+  private_subnets = each.value["private_subnets"]
 }
+
 
 #output "vpc" {
 #  value = module.vpc
