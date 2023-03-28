d2be4c0269d78beb1db3c4d0c247f94b12b140c1
sirisha517 <sai.sirifriends8790@gmail.com>
2023-03-28 17:01:37 +0530

    docdb

--- main.tf
+++ main.tf
@@ -1,30 +1,29 @@
 module "vpc" {
-  source              = "git::https://github.com/sirisha517/tf-module-vpc.git"
-  env                 = var.env
-  tags                = var.tags
-  default_route_table = var.default_route_table
-  default_vpc_id      = var.default_vpc_id
+  source                  = "git::https://github.com/sirisha517/tf-module-vpc.git"
+  env                     = var.env
+  tags                    = var.tags
+  default_route_table     = var.default_route_table
+  default_vpc_id          = var.default_vpc_id
 
-  for_each        = var.vpc
-  vpc_cidr        = each.value["vpc_cidr"]
-  public_subnets  = each.value["public_subnets"]
-  private_subnets = each.value["private_subnets"]
+  for_each                = var.vpc
+  vpc_cidr                = each.value["vpc_cidr"]
+  public_subnets          = each.value["public_subnets"]
+  private_subnets         = each.value["private_subnets"]
 }
 
 
 module "docdb" {
   source                  = "git::https://github.com/sirisha517/tf-module-db.git"
-  env                     = var.env
-  tags                    = var.tags
+  env    = var.env
+  tags   = var.tags
 
-  for_each                = var.vpc
+  subnet_ids = local.db_subnet_ids
+  vpc_id     = module.vpc["main"].vpc_id
+
+  for_each                = var.docdb
   engine                  = each.value["engine"]
   engine_version          = each.value["engine_version"]
   backup_retention_period = each.value["backup_retention_period"]
   preferred_backup_window = each.value["preferred_backup_window"]
   skip_final_snapshot     = each.value["skip_final_snapshot"]
-  subnet_ids              = local.db_subnet_ids
 }
-#output "vpc" {
-#  value = module.vpc
-#}
