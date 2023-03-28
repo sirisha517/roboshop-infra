573fc13b3f5e06b13bace42f57109d6d557110ba
sirisha517 <sai.sirifriends8790@gmail.com>
2023-03-28 16:29:35 +0530

    docdb

--- main.tf
+++ main.tf
@@ -16,14 +16,14 @@ module "docdb" {
   source                  = "git::https://github.com/sirisha517/tf-module-db.git"
   env                     = var.env
   tags                    = var.tags
-  subnet_ids              = local.db_subnet_ids
-  vpc_id                  = module.vpc["main"].vpc_id
+
   for_each                = var.vpc
   engine                  = each.value["engine"]
   engine_version          = each.value["engine_version"]
   backup_retention_period = each.value["backup_retention_period"]
   preferred_backup_window = each.value["preferred_backup_window"]
   skip_final_snapshot     = each.value["skip_final_snapshot"]
+  subnet_ids              = local.db_subnet_ids
 }
 #output "vpc" {
 #  value = module.vpc
