a88f25c8374f0ee134fdc4df6fd33f6ebe27c659
sirisha517 <sai.sirifriends8790@gmail.com>
2023-03-28 16:26:04 +0530

    docdb

--- locals.tf
+++ locals.tf
@@ -1,7 +1,6 @@
 locals {
-  subnet_ids = {
-    db  = tolist([module.vpc["main"].private_subnets["db-az1"].id, module.vpc["main"].private_subnets["db-az2"].id])
-    app = tolist([module.vpc["main"].private_subnets["app-az1"].id, module.vpc["main"].private_subnets["app-az2"].id])
-    web = tolist([module.vpc["main"].private_subnets["web-az1"].id, module.vpc["main"].private_subnets["web-az2"].id])
-  }
+  db  = tolist([module.vpc["main"].private_subnets["db-az1"].id, module.vpc["main"].private_subnets["db-az2"].id])
+  app = tolist([module.vpc["main"].private_subnets["app-az1"].id, module.vpc["main"].private_subnets["app-az2"].id])
+  web = tolist([module.vpc["main"].private_subnets["web-az1"].id, module.vpc["main"].private_subnets["web-az2"].id])
+
 }
