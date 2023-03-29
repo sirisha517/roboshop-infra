dac7a2331811632ded541d80986a4ad513c9af10
sirisha517 <sai.sirifriends8790@gmail.com>
2023-03-29 11:32:41 +0530

    docdb

--- main.tf
+++ main.tf
@@ -25,4 +25,6 @@ module "docdb" {
   backup_retention_period = each.value["backup_retention_period"]
   preferred_backup_window = each.value["preferred_backup_window"]
   skip_final_snapshot     = each.value["skip_final_snapshot"]
+  no_of_instances         = each.value["no_of_instances"]
+  instance_class          = each.value["instance_class"]
 }
