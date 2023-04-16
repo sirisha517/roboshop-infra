e96e5329ff0a1786dfc31affd8d77a5ecedc5783
sirisha517 <sai.sirifriends8790@gmail.com>
2023-04-16 12:10:27 +0530

    open application port

--- main.tf
+++ main.tf
@@ -75,13 +75,13 @@ module "alb" {
   source = "git::https://github.com/sirisha517/tf-module-alb.git"
   env    = var.env
   tags   = var.tags
-
+  vpc_id = module.vpc["main"].vpc_id
   for_each           = var.alb
   name               = each.value["name"]
   internal           = each.value["internal"]
   load_balancer_type = each.value["load_balancer_type"]
   subnets            = lookup(local.subnet_ids,each.value["subnet_name"],null)
-
+  allow_cidr         = each.value["allow_cidr"]
 }
 module "app" {
   source = "git::https://github.com/sirisha517/tf-module-app.git"
