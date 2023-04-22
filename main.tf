f19d624601d9e7371cf80e68f290afbe499cacf1
sirisha517 <sai.sirifriends8790@gmail.com>
2023-04-22 08:35:45 +0530

    open application port

--- main.tf
+++ main.tf
@@ -9,65 +9,65 @@ module "vpc" {
   public_subnets  = each.value["public_subnets"]
   private_subnets = each.value["private_subnets"]
 }
-//module "docdb" {
-//  source = "git::https://github.com/sirisha517/tf-module-docdb.git"
-//  env    = var.env
-//  tags   = var.tags
-//
-//  subnet_ids = local.db_subnet_ids
-//
-//  for_each                = var.docdb
-//  engine                  = each.value["engine"]
-//  engine_version          = each.value["engine_version"]
-//  backup_retention_period = each.value["backup_retention_period"]
-//  preferred_backup_window = each.value["preferred_backup_window"]
-//  skip_final_snapshot     = each.value["skip_final_snapshot"]
-//  no_of_instances         = each.value["no_of_instances"]
-//  instance_class          = each.value["instance_class"]
-//}
-//
-//module "rds" {
-//  source = "git::https://github.com/sirisha517/tf-module-rds.git"
-//  env    = var.env
-//  tags   = var.tags
-//
-//  subnet_ids = local.db_subnet_ids
-//
-//  for_each                = var.rds
-//  engine                  = each.value["engine"]
-//  backup_retention_period = each.value["backup_retention_period"]
-//  preferred_backup_window = each.value["preferred_backup_window"]
-//  engine_version          = each.value["engine_version"]
-//  instance_class          = each.value["instance_class"]
-//  no_of_instances         = each.value["no_of_instances"]
-//}
-//
-//module "elasticache" {
-//  source = "git::https://github.com/sirisha517/tf-module-elasticache.git"
-//  env    = var.env
-//  tags   = var.tags
-//
-//  subnet_ids = local.db_subnet_ids
-//
-//  for_each        = var.elasticache
-//  engine          = each.value["engine"]
-//  engine_version  = each.value["engine_version"]
-//  num_cache_nodes = each.value["num_cache_nodes"]
-//  node_type       = each.value["node_type"]
-//
-//}
-//
-//module "rabbitmq" {
-//  source = "git::https://github.com/sirisha517/tf-module-rabbitmq.git"
-//  env    = var.env
-//  tags   = var.tags
-//
-//  subnet_ids = local.db_subnet_ids
-//
-//  for_each      = var.rabbitmq
-//  instance_type = each.value["instance_type"]
-//
-//}
+module "docdb" {
+  source = "git::https://github.com/sirisha517/tf-module-docdb.git"
+  env    = var.env
+  tags   = var.tags
+
+  subnet_ids = local.db_subnet_ids
+
+  for_each                = var.docdb
+  engine                  = each.value["engine"]
+  engine_version          = each.value["engine_version"]
+  backup_retention_period = each.value["backup_retention_period"]
+  preferred_backup_window = each.value["preferred_backup_window"]
+  skip_final_snapshot     = each.value["skip_final_snapshot"]
+  no_of_instances         = each.value["no_of_instances"]
+  instance_class          = each.value["instance_class"]
+}
+
+module "rds" {
+  source = "git::https://github.com/sirisha517/tf-module-rds.git"
+  env    = var.env
+  tags   = var.tags
+
+  subnet_ids = local.db_subnet_ids
+
+  for_each                = var.rds
+  engine                  = each.value["engine"]
+  backup_retention_period = each.value["backup_retention_period"]
+  preferred_backup_window = each.value["preferred_backup_window"]
+  engine_version          = each.value["engine_version"]
+  instance_class          = each.value["instance_class"]
+  no_of_instances         = each.value["no_of_instances"]
+}
+
+module "elasticache" {
+  source = "git::https://github.com/sirisha517/tf-module-elasticache.git"
+  env    = var.env
+  tags   = var.tags
+
+  subnet_ids = local.db_subnet_ids
+
+  for_each        = var.elasticache
+  engine          = each.value["engine"]
+  engine_version  = each.value["engine_version"]
+  num_cache_nodes = each.value["num_cache_nodes"]
+  node_type       = each.value["node_type"]
+
+}
+
+module "rabbitmq" {
+  source = "git::https://github.com/sirisha517/tf-module-rabbitmq.git"
+  env    = var.env
+  tags   = var.tags
+
+  subnet_ids = local.db_subnet_ids
+
+  for_each      = var.rabbitmq
+  instance_type = each.value["instance_type"]
+
+}
 module "alb" {
   source = "git::https://github.com/sirisha517/tf-module-alb.git"
   env    = var.env
@@ -84,20 +84,20 @@ module "alb" {
 }
 
 module "app" {
-  source       = "git::https://github.com/sirisha517/tf-module-app.git"
-  env          = var.env
-  tags         = var.tags
-  bastion_cidr = var.bastion_cidr
-  vpc_id = module.vpc["main"].vpc_id
-  for_each         = var.app
-  component        = each.value["component"]
-  instance_type    = each.value["instance_type"]
-  desired_capacity = each.value["desired_capacity"]
-  max_size         = each.value["max_size"]
-  min_size         = each.value["min_size"]
-  subnets          = lookup(local.subnet_ids, each.value["subnet_name"], null)
-  port             = each.value["port"]
-  allow_app_to     = lookup(local.subnet_cidr, each.value["allow_app_to"], null)
+  source             = "git::https://github.com/sirisha517/tf-module-app.git"
+  env                = var.env
+  tags               = var.tags
+  bastion_cidr       = var.bastion_cidr
+  vpc_id             = module.vpc["main"].vpc_id
+  for_each           = var.app
+  component          = each.value["component"]
+  instance_type      = each.value["instance_type"]
+  desired_capacit    = each.value["desired_capacity"]
+  max_size           = each.value["max_size"]
+  min_size           = each.value["min_size"]
+  subnets            = lookup(local.subnet_ids, each.value["subnet_name"], null)
+  port               = each.value["port"]
+  allow_app_to       = lookup(local.subnet_cidr, each.value["allow_app_to"], null)
 }
 output "vpc" {
   value = module.vpc
