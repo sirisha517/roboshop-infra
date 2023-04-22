5650985943f760be587ce9e66299dab092908ffb
sirisha517 <sai.sirifriends8790@gmail.com>
2023-04-22 09:01:41 +0530

    open application port

--- main.tf
+++ main.tf
@@ -92,7 +92,7 @@ module "app" {
   for_each           = var.app
   component          = each.value["component"]
   instance_type      = each.value["instance_type"]
-  desired_capacit    = each.value["desired_capacity"]
+  desired_capacity    = each.value["desired_capacity"]
   max_size           = each.value["max_size"]
   min_size           = each.value["min_size"]
   subnets            = lookup(local.subnet_ids, each.value["subnet_name"], null)
