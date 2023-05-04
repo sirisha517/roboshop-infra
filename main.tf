13a2feedca269269ce9a73dba3f52493cf9f4e93
sirisha517 <sai.sirifriends8790@gmail.com>
2023-05-04 18:47:00 +0530

    payment fix

--- main.tf
+++ main.tf
@@ -69,18 +69,18 @@ module "elasticache" {
 }
 
 module "rabbitmq" {
-  source        = "git::https://github.com/sirisha517/tf-module-rabbitmq.git"
-  env    = var.env
-  tags   = var.tags
-  bastion_cidr       = var.bastion_cidr
-  dns_domain         = var.dns_domain
+  source             = "git::https://github.com/sirisha517/tf-module-rabbitmq.git"
+  env          = var.env
+  tags         = var.tags
+  bastion_cidr = var.bastion_cidr
+  dns_domain   = var.dns_domain
 
   subnet_ids = local.db_subnet_ids
-  vpc_id          = module.vpc["main"].vpc_id
+  vpc_id     = module.vpc["main"].vpc_id
 
   for_each      = var.rabbitmq
   instance_type = each.value["instance_type"]
-  allow_subnets   = lookup(local.subnet_cidr, each.value["allow_subnets"], null)
+  allow_subnets = lookup(local.subnet_cidr, each.value["allow_subnets"], null)
 
 }
 
